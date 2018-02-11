d5d1e40f36bf2a63922a7624102849cc30028e97
Mike Jaren Yap <mjaren.yap@gmail.com>
2018-02-11 13:53:59 +0800

    Set stylesheets and javascript templates and framworks
    I have provided the templates for both css and js files. I have also
    included my own css frameworks to use for features such as buttons,
    wrappers and such.
    
    I’m still trying to figure out the following:
    - fetching translated languages
    - animation features for both css and js
    - logo refining

---
+++ js/translate.js
@@ -0,0 +1,12 @@
+/*
+    script to configure language
+    calls language.json to fetch desired lang-mode
+*/
+
+$(document).ready(function(){
+    var dum;
+    $.get("json/dummy.json", function(data){
+        dum = data;
+        console.log(dum.username);
+    });
+});
