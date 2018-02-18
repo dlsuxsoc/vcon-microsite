0680ebaa81a05f119ff838cec3efdc018d1f3daf
Mike Jaren Yap <mjaren.yap@gmail.com>
2018-02-18 13:55:18 +0800

    Surface animations and responsive design
    - integrated a few animations to site
    - fixed issues with responsive designs

--- js/animate.js
+++ js/animate.js
@@ -1,1 +1,47 @@
 // animation assets for the vcon website
+
+$(document).ready(function(){
+
+    var stickyNavHeight = $("#sticky-nav").outerHeight();
+
+    // about section variables
+    var aboutWhatPosTop = $("#about-what-div").position().top - stickyNavHeight - 100;
+    var aboutWhoPosTop = $("#about-who-div").position().top - stickyNavHeight - 100;
+    var aboutWhyPosTop = $("#about-why-div").position().top - stickyNavHeight - 100;
+
+    //history section variables
+    var historyPosTop = $("#history").position().top - stickyNavHeight - 100;
+
+    $(window).scroll(function(){
+        previewAbout(aboutWhatPosTop, aboutWhoPosTop, aboutWhyPosTop);
+        previewHistory(historyPosTop);
+    });
+});
+
+function previewAbout(aboutWhatPosTop, aboutWhoPosTop, aboutWhyPosTop){
+    var windowScroll = $(this).scrollTop();
+
+    if(windowScroll >= aboutWhatPosTop){
+        $("#about-what-div").removeClass('invisible');
+        $("#about-what-div").addClass('show-visible');
+    }
+
+    if(windowScroll >= aboutWhoPosTop){
+        $("#about-who-div").removeClass('invisible');
+        $("#about-who-div").addClass('show-visible');
+    }
+
+    if(windowScroll >= aboutWhyPosTop){
+        $("#about-why-div").removeClass('invisible');
+        $("#about-why-div").addClass('show-visible');
+    }
+}
+
+function previewHistory(historyPosTop){
+    var windowScroll = $(this).scrollTop();
+
+    if(windowScroll >= historyPosTop){
+        $("#history").removeClass('invisible');
+        $("#history").addClass('show-visible');
+    }
+}
